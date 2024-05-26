e848d1737b95b55f897b3e4cfb696f941c77e3d8
moneydiaries <moneydiaries.ref29@gmail.com>
2024-05-26 21:03:46 +0000

    add magedemo

---
+++ magedemo/data_loaders/load_from_api.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your data loading logic here
+    url = "https://raw.githubusercontent.com/el-grudge/magetalk/main/data/flight_data_012024.csv"
+
+    return pd.read_csv(url)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
